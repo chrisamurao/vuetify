f780c420eb3d512d68ac55969a87ae460323d142
Christopher Amurao <camurao@csatf.org>
2019-04-26 16:28:34 -0700

    finished up til form validation, continue here https://www.youtube.com/watch?v=_-5aD1VXj-s

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,21 @@
         <span class="font-weight-light">MyNameIs</span>
         <span>Tipper</span>
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
+
+      <v-menu offset-y>
+        <v-btn flat slot="activator" color="grey">
+          <v-icon>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-title>{{link.text}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
       <v-btn flat color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
@@ -20,6 +34,9 @@
             <img class="text-lg-center" src="/avatar-1.png">
           </v-avatar>
           <p class="white--text subheading mt-1">The Tip Tapper</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup></Popup>
         </v-flex>
       </v-layout>
       <v-list>
@@ -37,7 +54,11 @@
 </template>
 
 <script>
+import Popup from "./Popup";
 export default {
+  components: {
+    Popup
+  },
   data() {
     return {
       drawer: false,
